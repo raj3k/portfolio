2e9a4cd1351edc0fc2afa698958fe84dce38088b
raj3k <rajkosky@gmail.com>
2023-02-20 23:17:29 +0100

    add Timeline & TimelineItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Background from "./components/Background";
 import Introduction from "./components/Introduction";
 import Footer from "./components/Footer";
+import Timeline from "./components/Timeline";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Background />
       <div className="max-w-5xl w-11/12 mx-auto">
         <Introduction />
+        <Timeline />
         <Footer />
       </div>
     </main>

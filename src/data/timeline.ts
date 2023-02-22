ce9c984efff657c5ad22264cdcab363ff608cf2d
raj3k <rajkosky@gmail.com>
2023-02-22 23:28:43 +0100

    data for timeline

---
+++ src/data/timeline.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    timeInterval: "1 March 2020 - Present",
+    title: "Reference Data Analyst",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
+  },
+  {
+    timeInterval: "2013 - 2017",
+    title: "IT student",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
+  },
+  {
+    timeInterval: "2008 - 2012",
+    title: "High School",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
+  }
+]

7e02a33a428fb1ab4dd3e6bfd05c1d6a907f4619
raj3k <rajkosky@gmail.com>
2023-02-23 15:25:11 +0100

    data for projects and passing Portfolio component to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Background from "./components/Background";
 import Introduction from "./components/Introduction";
 import Footer from "./components/Footer";
 import Timeline from "./components/Timeline";
+import Portfolio from "./components/Portfolio";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Background />
       <div className="max-w-5xl w-11/12 mx-auto">
         <Introduction />
+        <Portfolio />
         <Timeline />
         <Footer />
       </div>
---
+++ src/data/portfolio.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Random Kanye West Quote',
+    imgUrl: '/assets/random-kanye-quote.png',
+    stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+    link: 'http://random-kanye-quote.s3-website.eu-central-1.amazonaws.com/',
+  },
+  {
+    title: 'Quiz Application',
+    imgUrl: '/assets/quiz.png',
+    stack: ['Python', 'Django', 'Docker'],
+    link: 'https://github.com/raj3k/quiz_app',
+  },
+  {
+    title: 'Image Converter using Microservice architecture',
+    imgUrl: '/assets/system-diagram.png',
+    stack: ['NodeJS', 'TypeScript', 'Python', 'RabbitMQ', 'MongoDB', 'MySQL', 'Docker'],
+    link: 'https://github.com/raj3k/RGB-to-Grayscale-MICROSERVICES',
+  },
+]

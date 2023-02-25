bb6c4347b36851fe54f4795098d479cfff7206cb
raj3k <rajkosky@gmail.com>
2023-02-25 12:17:18 +0100

    modified introduction and footer

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,7 +8,7 @@ const Introduction = () => {
         <p className="text-base md:text-xl mb-3 text-white font-medium">Software Engineer & Web Developer</p>
         <p className="text-sm max-w-xl mb-6 text-white font-bold">
           <p className="py-1.5">I'm a software engineer from Poland.</p>
-          <p className="py-1.5">I enjoy working on application backend but frontend is no stranger to me.</p>
+          <p className="py-1.5">I enjoy working on application backend.</p>
           <p className="py-1.5">I have particular interest in cloud computing technology and trying to take my first steps into it.</p>
           <p className="py-1.5">For work, I'm a Analyst at corporation from banking sector, where I work in international environment, helping to
           upskill and mentor colleagues with less experience. I'm responsible for automation in Excel spreadsheets and cooperating with Dev Team.</p>
